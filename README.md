aa501f9dc4f056e3ab36e25833747b79da3d8c6a
Stone Tao <stonezt2019@gmail.com>
2020-08-30 20:35:48 -0700

    readme and fake tournament setup

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 This repository will hold the game logic file (the Design), all the public Docker images bots are run on, and starter kits for every language
 
 Change this name some time later
+
+## Getting Started
+
+Install the design with
+
+```
+npm install -g lux-ai-challenge-2020
+```
+
+To run a match from command line, simply run
+
+```
+lux-ai-run path/to/bot path/to/otherbot
+```
+
+and the match will run with some logging and store error logs and a replay in a new `errorlogs` folder and `replays` folder
+
+The `kits` folder in this repository holds all of the available starter its you can use to start competing and building an AI agent.
+
---
+++ tests/tournament.spec.ts
@@ -0,0 +1,28 @@
+import * as Dimensions from 'dimensions-ai';
+import { LuxDesign } from '../src';
+import { Logger, Tournament } from 'dimensions-ai';
+import { TournamentType } from 'dimensions-ai/lib/Tournament/TournamentTypes';
+const design = new LuxDesign('Lux Design');
+const luxdim = Dimensions.create(design, {
+  name: 'luxdimension',
+  id: 'luxdim',
+  defaultMatchConfigs: {
+
+  },
+  loggingLevel: Logger.LEVEL.INFO,
+  secureMode: true,
+  observe: true,
+  activateStation: true
+});
+
+const botList = [];
+const tourney = luxdim.createTournament(botList, {
+  rankSystem: Tournament.RankSystem.TRUESKILL,
+  type: TournamentType.LADDER,
+  resultHandler: LuxDesign.resultHandler,
+  agentsPerMatch: [2],
+  consoleDisplay: true,
+  name: 'Lux Tournament',
+  id: 'luxtourney'
+});
+tourney.run();
