ba30adc18268155eb07f32bd3de298e907d369a8
Stone Tao <stonezt2019@gmail.com>
2020-09-06 11:34:11 -0700

    fix nasty import cycle

--- kits/js/bot.js
+++ kits/js/bot.js
@@ -5,8 +5,8 @@ const agent = new kit.Agent();
 
 // first initialize the agent, and then proceed to go in a loop waiting for updates and running the AI
 agent.initialize().then(async () => {
-  while(true) {
-    
+  while (true) {
+
     // wait for update from match engine
     await agent.update();
 
@@ -15,14 +15,13 @@ agent.initialize().then(async () => {
     let commands = [];
 
     // push some commands in to be processed by the `MatchEngine` working under a `Design`
-    commands.push('somecommand');
-    commands.push('anothercommand');
+    commands.push('m u_0 s');
 
     // submit commands to the `MatchEngine` and the `Match`, using ',' as the delimiter
     console.log(commands.join(','));
-    
+
     // now we end our turn
     agent.endTurn();
-    
+
   }
 });
--- src/Unit/index.ts
+++ src/Unit/index.ts
@@ -1,7 +1,10 @@
 import { LuxMatchConfigs } from '../types';
+import { Game } from '../Game';
+import { MoveAction, TransferAction, SpawnCityAction } from '../Actions';
+import { MatchWarn } from 'dimensions-ai';
 import { Actionable } from '../Actionable';
-import { Position } from '../GameMap/position';
 import { Resource } from '../Resource';
+import { Position } from '../GameMap/position';
 
 export abstract class Unit extends Actionable {
   public id: string;
@@ -93,4 +96,83 @@ export namespace Unit {
   export type Cargo = {
     [x in Resource.Types]: number;
   };
+}
+
+export class Cart extends Unit {
+  constructor(x: number, y: number, team: Unit.TEAM, configs: LuxMatchConfigs) {
+    super(x, y, Unit.Type.CART, team, configs);
+  }
+  getLightUpkeep(): number {
+    return this.configs.parameters.LIGHT_UPKEEP.WORKER;
+  }
+
+  canMove(): boolean {
+    return this.cooldown === 0;
+  }
+
+  turn(game: Game): void {
+    if (this.cooldown > 0) {
+      this.cooldown--;
+    }
+    if (this.currentActions.length === 1) {
+      const action = this.currentActions[0];
+      if (action instanceof MoveAction) {
+        game.moveUnit(action.team, action.unitid, action.direction);
+        this.cooldown += this.configs.parameters.UNIT_ACTION_COOLDOWN.CART;
+      } else if (action instanceof TransferAction) {
+        game.transferResources(
+          action.team,
+          action.srcID,
+          action.destID,
+          action.resourceType,
+          action.amount
+        );
+      }
+    } else if (this.currentActions.length > 1) {
+      throw new MatchWarn(
+        `Agent ${this.team} tried to run more than 1 action for cart: ${this.id}`
+      );
+    }
+  }
+}
+
+export class Worker extends Unit {
+  constructor(x: number, y: number, team: Unit.TEAM, configs: LuxMatchConfigs) {
+    super(x, y, Unit.Type.WORKER, team, configs);
+  }
+  getLightUpkeep(): number {
+    return this.configs.parameters.LIGHT_UPKEEP.WORKER;
+  }
+
+  canMove(): boolean {
+    return this.cooldown === 0;
+  }
+
+  turn(game: Game): void {
+    if (this.cooldown > 0) {
+      this.cooldown--;
+    }
+    if (this.currentActions.length === 1) {
+      const action = this.currentActions[0];
+      if (action instanceof MoveAction) {
+        game.moveUnit(action.team, action.unitid, action.direction);
+        this.cooldown += this.configs.parameters.UNIT_ACTION_COOLDOWN.WORKER;
+      } else if (action instanceof TransferAction) {
+        game.transferResources(
+          action.team,
+          action.srcID,
+          action.destID,
+          action.resourceType,
+          action.amount
+        );
+      } else if (action instanceof SpawnCityAction) {
+        game.spawnCityTile(action.team, this.pos.x, this.pos.y);
+        this.cooldown += this.configs.parameters.UNIT_ACTION_COOLDOWN.WORKER;
+      }
+    } else if (this.currentActions.length > 1) {
+      throw new MatchWarn(
+        `Agent ${this.team} tried to run more than 1 action for worker: ${this.id}`
+      );
+    }
+  }
 }
