835fb518af9dd4a86a785d49e80949719d7a6cd5
Stone Tao <stonezt2019@gmail.com>
2021-06-19 21:52:01 -0400

    init

--- kits/js/simple/lux/kit.js
+++ kits/js/simple/lux/kit.js
@@ -80,8 +80,6 @@ class Agent {
     let mapInfo = (await this.getLine());
     let width = mapInfo.nextInt();
     let height = mapInfo.nextInt();
-    this.mapWidth = width;
-    this.mapHeight = height;
     const map = new GameMap(width, height);
     const players = [new Player(0), new Player(1)];
 
@@ -114,7 +112,7 @@ class Agent {
   async retrieveUpdates() {
     this.resetPlayerStates();
     // TODO: this can be optimized. we only reset because some resources get removed
-    this.gameState.map = new GameMap(this.mapWidth, this.mapHeight);
+    this.gameState.map = new GameMap(this.gameState.map.width, this.gameState.map.height);
     while (true) {
       let update = (await this.getLine());
       if (update.str === INPUT_CONSTANTS.DONE) {
