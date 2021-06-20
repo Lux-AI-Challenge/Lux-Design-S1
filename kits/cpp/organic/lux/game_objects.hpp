ef07d99169ecf7a36bec74fcce441354add4e209
Stone Tao <stonezt2019@gmail.com>
2021-06-19 22:43:34 -0400

    fix bugs

--- kits/cpp/organic/lux/game_objects.hpp
+++ kits/cpp/organic/lux/game_objects.hpp
@@ -66,7 +66,7 @@ namespace lux
         /** whether or not the unit can build where it is right now */
         bool canBuild(const GameMap &gameMap) {
             auto cell = gameMap.getCellByPos(this->pos);
-            if (!cell->hasResource()) {
+            if (!cell->hasResource() && this->cooldown < 1) {
                 // TODO: future, check cargo?
                 return true;
             }
