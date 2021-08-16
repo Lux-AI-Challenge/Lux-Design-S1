6b5fe9027520013bb244be26b7bf4529d77f4dd8
Stone Tao <stonezt2019@gmail.com>
2021-08-16 11:07:40 -0400

    fix bug with python kit `id` and repackage kits

--- kits/python/simple/agent.py
+++ kits/python/simple/agent.py
@@ -27,6 +27,7 @@ def agent(observation, configuration):
         game_state = Game()
         game_state._initialize(observation["updates"])
         game_state._update(observation["updates"][2:])
+        game_state.id = observation.player
     else:
         game_state._update(observation["updates"])
     
