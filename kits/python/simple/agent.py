7229a46880c7cb7656a6380d200ca206fd606edc
Stone Tao <stonezt2019@gmail.com>
2021-08-21 00:51:47 -0400

    fix imports

--- kits/python/simple/agent.py
+++ kits/python/simple/agent.py
@@ -1,19 +1,9 @@
 import math, sys
-
-if __package__ == "":
-    # for kaggle-environments
-    from lux.game import Game
-    from lux.game_map import Cell, RESOURCE_TYPES
-    from lux.constants import Constants
-    from lux.game_constants import GAME_CONSTANTS
-    from lux import annotate
-else:
-    # for CLI tool
-    from .lux.game import Game
-    from .lux.game_map import Cell, RESOURCE_TYPES
-    from .lux.constants import Constants
-    from .lux.game_constants import GAME_CONSTANTS
-    from .lux import annotate
+from lux.game import Game
+from lux.game_map import Cell, RESOURCE_TYPES
+from lux.constants import Constants
+from lux.game_constants import GAME_CONSTANTS
+from lux import annotate
 
 DIRECTIONS = Constants.DIRECTIONS
 game_state = None
