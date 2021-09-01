6c28a2c2950a26af6c8d3189f93a27ba3c9bef8f
stone_tao <stonezt2019@gmail.com>
2021-08-31 22:13:32 -0400

    update kit constants and packaging

--- kits/java/simple/lux/GameConstants.java
+++ kits/java/simple/lux/GameConstants.java
@@ -16,12 +16,12 @@ public final class GameConstants {
       public static final int NIGHT_LENGTH = 10;
       public static final int MAX_DAYS = 360;
       public static class LIGHT_UPKEEP {
-        public static final int CITY = 30;
+        public static final int CITY = 23;
         public static final int WORKER = 4;
         public static final int CART = 10;
       }
-      public static final double WOOD_GROWTH_RATE = 1.01;
-      public static final int MAX_WOOD_AMOUNT = 400;
+      public static final double WOOD_GROWTH_RATE = 1.025;
+      public static final int MAX_WOOD_AMOUNT = 500;
       public static final int CITY_ADJACENCY_BONUS = 5;
       public static final int CITY_BUILD_COST = 100;
       public static class RESOURCE_CAPACITY {
@@ -30,13 +30,13 @@ public final class GameConstants {
       }
       public static class WORKER_COLLECTION_RATE {
         public static final int WOOD = 20;
-        public static final int COAL = 10;
-        public static final int URANIUM = 4;
+        public static final int COAL = 5;
+        public static final int URANIUM = 2;
       }
       public static class RESOURCE_TO_FUEL_RATE {
         public static final int WOOD = 1;
-        public static final int COAL = 5;
-        public static final int URANIUM = 20;
+        public static final int COAL = 10;
+        public static final int URANIUM = 40;
       }
       public static class RESEARCH_REQUIREMENTS {
         public static final int COAL = 50;
@@ -49,7 +49,7 @@ public final class GameConstants {
       }
       public static final double MAX_ROAD = 6;
       public static final double MIN_ROAD = 0;
-      public static final double CART_ROAD_DEVELOPMENT_RATE = 0.5;
+      public static final double CART_ROAD_DEVELOPMENT_RATE = 0.75;
       public static final double PILLAGE_RATE = 0.5;
     }
 }
