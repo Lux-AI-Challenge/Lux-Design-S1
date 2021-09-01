b2c8f6c4f96e636502d791b22d76097632145713
Sergey Tolmachev <tolsi.ru@gmail.com>
2021-09-01 12:49:08 +0300

    Update to V3.1.x

--- kits/kotlin/simple/bot/src/main/kotlin/luxaibot/lux/GameConstants.kt
+++ kits/kotlin/simple/bot/src/main/kotlin/luxaibot/lux/GameConstants.kt
@@ -17,18 +17,18 @@ class GameConstants {
         const val DAY_LENGTH = 30
         const val NIGHT_LENGTH = 10
         const val MAX_DAYS = 360
-        const val WOOD_GROWTH_RATE = 1.01
-        const val MAX_WOOD_AMOUNT = 400
+        const val WOOD_GROWTH_RATE = 1.025
+        const val MAX_WOOD_AMOUNT = 500
         const val CITY_ADJACENCY_BONUS = 5
         const val CITY_BUILD_COST = 100
         const val CITY_ACTION_COOLDOWN = 10
         const val MAX_ROAD = 6.0
         const val MIN_ROAD = 0.0
-        const val CART_ROAD_DEVELOPMENT_RATE = 0.5
+        const val CART_ROAD_DEVELOPMENT_RATE = 0.75
         const val PILLAGE_RATE = 0.5
 
         object LIGHT_UPKEEP {
-            const val CITY = 30
+            const val CITY = 23
             const val WORKER = 4
             const val CART = 10
         }
@@ -40,14 +40,14 @@ class GameConstants {
 
         object WORKER_COLLECTION_RATE {
             const val WOOD = 20
-            const val COAL = 10
-            const val URANIUM = 4
+            const val COAL = 5
+            const val URANIUM = 2
         }
 
         object RESOURCE_TO_FUEL_RATE {
             const val WOOD = 1
-            const val COAL = 5
-            const val URANIUM = 20
+            const val COAL = 10
+            const val URANIUM = 40
         }
 
         object RESEARCH_REQUIREMENTS {
